d055878ec65e89ffa47b560df8bd083c74828898
Planetarium <planetarium1001@163.com>
2025-04-09 14:16:27 +0800

    Update

--- sync.py
+++ sync.py
@@ -14,36 +14,52 @@ custom = custom.split("\n")
 def copy_files(home, root, option):
     if option == 0: # Push
         for part in home:
-            if not os.path.exists("./" + part[0]):
-                os.mkdir("./" + part[0])
+            if part[1][0] == "-rf": # Folder
+                path = part[0]
+            elif part[1][0] == "-f": # File
+                path = part[0].replace(part[0].split("/")[-1], "")
+            if not os.path.exists("./" + path):
+                os.makedirs("./" + path)
             # cp config/fcitx5/~/.config/fcitx5/['-rf', '*'] ./config/fcitx5/
             print("cp " + str(part[1][0]) + " ~/." + str(part[0]) + str(part[1][1]) + " ./" + str(part[0]))
             cmd = "cp " + part[1][0] + " ~/." + part[0] + part[1][1] + " ./" + part[0]
             os.system(cmd)
         for part in root:
-            if not os.path.exists("." + part[0]):
-                os.mkdir("." + part[0])
-            cmd = "cp " + part[1][0] + " " + part[0] + part[1][1] + " ." + part[0]
+            if part[1][0] == "-rf": # Folder
+                path = part[0]
+            elif part[1][0] == "-f": # File
+                path = part[0].replace(part[0].split("/")[-1], "")
+            if not os.path.exists("./root" + path):
+                os.makedirs("./root" + path)
+            cmd = "cp " + part[1][0] + " " + part[0] + part[1][1] + " ./root" + part[0]
             os.system(cmd)
     elif option == 1: # Pull
         for part in home:
-            if not os.path.exists("~/." + part[0]):
-                os.mkdir("~/." + part[0])
+            if part[1][0] == "-rf": # Folder
+                path = part[0]
+            elif part[1][0] == "-f": # File
+                path = part[0].replace(part[0].split("/")[-1], "")
+            if not os.path.exists("~/." + path):
+                os.makedirs("~/." + path)
             cmd = "cp " + part[1][0] + " ./" + part[0]+ part[1][1] + " ~/." + part[0] 
             os.system(cmd)
         cmd = ""
         for part in root:
             if part[1][1] == "*":
-                diff_cmd = "diff -r " + part[0] + " " + "." + part[0] + " > /tmp/diff"
+                diff_cmd = "diff -r " + part[0] + " " + "./root" + part[0] + " > /tmp/diff"
             else:
-                diff_cmd = "diff " + part[0] + " " + "." + part[0] + " > /tmp/diff"
-            if not os.path.exists("." + part[0]):
-                cmd += "sudo mkdir -p " + part[0] + " && "
+                diff_cmd = "diff " + part[0] + " " + "./root" + part[0] + " > /tmp/diff"
+            if part[1][0] == "-rf": # Folder
+                path = part[0]
+            elif part[1][0] == "-f": # File
+                path = part[0].replace(part[0].split("/")[-1], "")
+            if not os.path.exists("." + path):
+                cmd += "sudo mkdir -p " + path + " && "
                 diff_cmd = "echo 'not exists' > /tmp/diff"
             with open("/tmp/diff", "r") as d_r:
                 diff = d_r.read()
             if diff != "":
-                cmd += "sudo cp " + part[1][0] + " ." + part[0]+ part[1][1] + " " + part[0] + " && "
+                cmd += "sudo cp " + part[1][0] + " ./root" + part[0]+ part[1][1] + " " + part[0] + " && "
         if cmd != "":
             print("Need sudo: ")
             os.system(cmd)
